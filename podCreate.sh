7af6d3703965963a3fa0bcaee5c4c19bb522e852
Kristopher Amiel A Reyes <kristopheramielareyes@Kristophers-Untrusted-connection.local>
2015-11-09 18:29:46 +0800

    update commit

--- podCreate.sh
+++ podCreate.sh
@@ -1,3 +1,27 @@
-1. Create repository
-2. create pod
-3.
+#!/bin/bash
+# create Private Pods
+clear
+echo "Please Enter [POD_NAME]"
+read podName
+echo "your pod name: $podName"
+pod lib create $podName
+echo "Enter Your Repository Link"
+read repLink
+echo "YOUR REPOSITORY LINK: $repLink"
+echo "PLEASE UPDATE YOU PODSPEC are you done, Please Press a to continue"
+read -n 1
+git init
+git remote add origin $repLink
+git add -A
+git commit -m "Initial Commit"
+echo "Enter your Version:"
+read versionTag
+echo "Version: $versionTag"
+git tag -a $versionTag -m "Version $versionTag"
+git push -u origin master
+
+
+
+
+
+
